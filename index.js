8ff9ee4ce68a81c23f980099b017eb0a3872d723
Mathis Lefranc <lefrancmathis@gmail.com>
2022-06-25 19:12:14 -0400

    added copy to clipboard script

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+// Toggle Menu Button Script
+
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+// Copy to clipboard Script
+
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
